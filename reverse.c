124c9fa60d9f39bae3871acb4644de645660da77
haoyu0970624763 <haoyu0970624763@gmail.com>
2022-07-26 09:52:16 +0800

    Add library and typedef to avoid warning

--- reverse.c
+++ reverse.c
@@ -1,10 +1,19 @@
-void *reverse(node_t **head)
+#include <stdio.h>
+
+typedef struct __node {
+    int value;
+    struct __node *next;
+} node_t;
+
+void reverse(node_t **head)
 {
     node_t *cursor = NULL;
     while (*head) {
         node_t *next = (*head)->next;
-        (*head)->next = cursor; cursor = *head;
+        (*head)->next = cursor; 
+        cursor = *head;
         (*head) = next;
     }
     *head=cursor;
 }
+
--- swap_pair.c
+++ swap_pair.c
@@ -1,4 +1,9 @@
-void *swap_pair(node_t **head)
+typedef struct __node {
+    int value;
+    struct __node *next;
+} node_t;
+
+void swap_pair(node_t **head)
 {
     for (node_t **node = head; *node && (*node)->next; node = &(*node)->next->next) {
         node_t *tmp = *node;
